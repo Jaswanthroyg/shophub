a75877e8a72949be2881110f15b9c084f9184acc
roy24 <jaswanthroyg1@gmail.com>
2026-07-20 15:50:18 +0530

    feat: add Category model and Django admin integration

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class category(models.Model):
+class Category(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField()
     image=models.ImageField(upload_to='category_images/')

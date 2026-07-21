9813fde645100c1067190c4bf7673cb6f0dc9697
roy24 <jaswanthroyg1@gmail.com>
2026-07-21 19:47:11 +0530

    feat: implement category create API

--- categories/models.py
+++ categories/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Category(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField()
-    image=models.ImageField(upload_to='category_images/')
+    image=models.ImageField(upload_to='category_images/',null=True, blank=True)
     is_active=models.BooleanField(default=True)
 
---
+++ categories/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import CategoryAPIView
+
+urlpatterns = [
+    path(
+        "",
+        CategoryAPIView.as_view(),
+        name="category-list-create"
+    ),
+]

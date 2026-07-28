3f53fc0239e4c204b153db53ff80bb8ccad921e8
roy24 <jaswanthroyg1@gmail.com>
2026-07-28 16:51:34 +0530

    feat:implements the products list api

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import ProductCreateAPIView
+from .views import ProductCreateAPIView, ProductListAPIView
 
 
 urlpatterns=[
     path("create/",ProductCreateAPIView.as_view(),name="product-create"),
+    path("",ProductListAPIView.as_view(),name="product-List")
 ]
--- products/views.py
+++ products/views.py
@@ -19,3 +19,9 @@ class ProductCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProductListAPIView(APIView):
+    def get(self,request):
+        products=Product.objects.all()
+        serializer=ProductSerializer(products,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    

6c146524c59e45d4e7d0a48bb84c16958c6f12f5
roy24 <jaswanthroyg1@gmail.com>
2026-07-28 15:45:36 +0530

    feat: implement product create API

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "name",
+        "price",
+        "stock",
+        "brand",
+        "category",
+        "is_active",
+
+    )
+    search_fields=(
+        "name",
+        "brand"
+    )
+    list_filter=(
+        "is_active",
+        "category",
+        "brand"
+    )

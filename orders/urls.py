86a36dd2534883be767e3be111f667efbdee16f6
roy24 <jaswanthroyg1@gmail.com>
2026-07-31 19:44:09 +0530

    feat: implement order cancellation API with business validation

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CheckoutAPIView,OrderListAPIView,OrderDetailView
+from .views import CheckoutAPIView,OrderListAPIView,OrderDetailView,CancelOrderAPIView
 
 
 urlpatterns=[
     path("checkout/",CheckoutAPIView.as_view(),name="checkout"),
     path("",OrderListAPIView.as_view(),name="order_list"),
-    path("<int:pk>/",OrderDetailView.as_view(),name="order_detail")
+    path("<int:pk>/",OrderDetailView.as_view(),name="order_detail"),
+    path("<int:pk>/cancel/",CancelOrderAPIView.as_view(),name="cancel-order")
 ]
--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 from cart.models import Cart
 from .models import Order, OrderItem
@@ -95,3 +96,25 @@ class OrderDetailView(APIView):
                                 )
         serializer=OrderDetailSerializer(order)
         return Response(serializer.data)
+
+class CancelOrderAPIView(APIView):
+    permission_classes=[IsAuthenticated]
+    def patch(self,request,pk):
+        order=get_object_or_404(
+            Order,
+            id=pk,
+            user=request.user
+        )
+        if order.status in [
+            "shipped",
+            "delivered",
+            "cancelled"
+        ]:
+            return Response({
+                "error":"This order can not be cancelled."
+            },status=status.HTTP_400_BAD_REQUEST)
+        order.status="cancelled"
+        order.save()
+        return Response({
+            "message":"Order Cancelled Successfully."
+        },status=status.HTTP_200_OK)

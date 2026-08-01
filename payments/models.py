a58381fa249ba7ed8e9639850083888ce35cbb1a
roy24 <jaswanthroyg1@gmail.com>
2026-08-01 12:58:46 +0530

    feat: initialize payment module and create payment model

---
+++ payments/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from orders.models import Order
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+        ("Failed", "Failed"),
+    ]
+
+    PAYMENT_METHOD_CHOICES = [
+        ("UPI","UPI"),
+        ("Card","Card"),
+        ("Net Banking","Net Banking"),
+        ("Wallet","Wallet"),
+    ]
+    order=models.OneToOneField(Order,
+                               on_delete=models.CASCADE,
+                            related_name='payment')
+    amount=models.DecimalField(max_digits=10,decimal_places=2)
+
+    status=models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default="Pending"   
+    )
+    payment_method=models.CharField(
+        max_length=20,
+    choices=PAYMENT_METHOD_CHOICES,
+    blank=True,
+    )
+    gateway_payment_id=models.CharField(max_length=100,
+                                        blank=True,
+                                        null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment for Order{self.order.id}-{self.status}"

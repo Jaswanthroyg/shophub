49a40c9b7ca57be5c6cf238bf604fd8d0f9d8045
roy24 <jaswanthroyg1@gmail.com>
2026-09-02 14:03:42 +0530

    Fix product API routes

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ urlpatterns=[
     path("create/",ProductCreateAPIView.as_view(),name="product-create"),
     path("",ProductListAPIView.as_view(),name="product-List"),
     path("<int:id>/",ProductDetailAPIView.as_view(),name="product-detail"),
-    path("<int:id>/",ProductUpdateAPIView.as_view(),name="product-update"),
-    path("<int:id>/",ProductDeleteAPIVIew.as_view(),name="product-delete"),
+    path("<int:id>/update/",ProductUpdateAPIView.as_view(),name="product-update"),
+    path("<int:id>/delete/",ProductDeleteAPIVIew.as_view(),name="product-delete"),
 ]
